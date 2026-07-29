1eea73b7e004f6750a297448dc38b9f1d516aee2
VANI V <vanivani1659@gmail.com>
2026-07-29 16:08:54 +0530

    Day 2: Start Python comparison operators

---
+++ PYTHON/Day 2/comparison_operators.py
@@ -0,0 +1,8 @@
+# #Comparison operators are used to compare two values. They return a boolean value (True or False) based on comparison.
+# Common xamples of comparison operators in Python include:
+# 1. Equal to (==): Returns True if the values on both sides are equal.
+# 2. Not equal to (!=): Returns True if the values on both sides are not equal.
+# 3. Greater than (>): Returns True if the value on the left is greater than the value on the right.
+# 4. Less than (<): Returns True if the value on the left is less than the value on the right.
+# 5. Greater than or equal to (>=): Returns True if the value on the left is greater than or equal to the value on the right.
+# 6. Less than or equal to (<=): Returns True if the value on the left is less than or equal to the value on the right.
