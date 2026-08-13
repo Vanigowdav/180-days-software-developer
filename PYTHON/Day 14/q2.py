16a69ed152e77f2b158a3947a63aeb390ad38c08
VANI V <vanivani1659@gmail.com>
2026-08-13 19:19:19 +0530

    Day 14 : Solved basic questions

---
+++ PYTHON/Day 14/q2.py
@@ -0,0 +1,6 @@
+# if-elif-else
+n = int(input("Enter the number: "))
+if n % 2 !=0 or (n % 2 ==0 and 6<=n<=20):
+    print("Weird")
+else:
+    print("Not Weird")
---
+++ PYTHON/Day 14/q4.py
@@ -0,0 +1,4 @@
+# Task :The provided code stub reads an integer, , from STDIN. For all non-negative integers i < n , print i^2
+n = int(input("Enter the number:"))
+for i in range(n):
+    print(i * i)
